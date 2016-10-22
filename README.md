7f10089e1a24ebba8490fa6521ba40241f316f6a
jjYBdx4IL <jjYBdx4IL@github.com>
2016-10-22 23:19:19 +0200

    prevent blocking tests from executing in CI environments and when running test batches in general

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # audio-examples
+
+Remark: the tests are interactive only because of their blocking nature. Only specific test execution will actually make them
+run, ie.
+
+  mvn test -Dtest=<class>#<testMethod>
+
+or selecting "run focused test method" in Netbeans or the equivalent in Eclipse.
 --
 [![Build Status](https://travis-ci.org/jjYBdx4IL/audio-examples.png?branch=master)](https://travis-ci.org/jjYBdx4IL/audio-examples)
-devel/java/github/audio-examples@7078
+devel/java/github/audio-examples@7081
--- src/test/java/com/github/jjYBdx4IL/audio/examples/AudioInputOutputBase.java
+++ src/test/java/com/github/jjYBdx4IL/audio/examples/AudioInputOutputBase.java
@@ -1,5 +1,7 @@
 package com.github.jjYBdx4IL.audio.examples;
 
+import com.github.jjYBdx4IL.utils.env.Surefire;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
@@ -12,6 +14,8 @@ import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,12 +25,67 @@ import org.slf4j.LoggerFactory;
  */
 public class AudioInputOutputBase {
 
+
     private static final Logger LOG = LoggerFactory.getLogger(AudioInputOutputBase.class);
     public static final float SAMPLE_RATE = 44100f;
     public static final int CHANNELS = 2;
     public static final int SAMPLE_SIZE_BITS = 16;
     public static final boolean BIG_ENDIAN = true;
     public static final int FRAME_SIZE = SAMPLE_SIZE_BITS * CHANNELS / 8;
+    @BeforeClass
+    public static void beforeClass() {
+        Assume.assumeTrue(Surefire.isSingleTextExecution());
+    }
+    /**
+     * Write a normalized (-1..+1) amplitude sample to the left channel.
+     *
+     * @param buf
+     * @param frame
+     * @param value
+     */
+    public static void putLeft(byte[] buf, int frame, double value) {
+        if (Math.abs(value) > 1d) {
+            throw new IllegalArgumentException();
+        }
+        long wordValue = (long) (value * 32767d);
+        buf[frame * FRAME_SIZE + 0] = (byte) (wordValue >> 8);
+        buf[frame * FRAME_SIZE + 1] = (byte) (wordValue & 0xFF);
+    }
+    /**
+     * Write a normalized (-1..+1) amplitude sample to the right channel.
+     *
+     * @param buf
+     * @param frame
+     * @param value
+     */
+    public static void putRight(byte[] buf, int frame, double value) {
+        if (Math.abs(value) > 1d) {
+            throw new IllegalArgumentException();
+        }
+        long wordValue = (long) (value * 32767d);
+        buf[frame * FRAME_SIZE + 2] = (byte) (wordValue >> 8);
+        buf[frame * FRAME_SIZE + 3] = (byte) (wordValue & 0xFF);
+    }
+    /**
+     * Return the normalized (-1..+1) amplitude sample for the left channel.
+     *
+     * @param buf
+     * @param frame
+     * @return
+     */
+    public static double getLeft(byte[] buf, int frame) {
+        return (buf[frame * FRAME_SIZE + 0] << 8) | buf[frame * FRAME_SIZE + 1] & 0xFF;
+    }
+    /**
+     * Return the normalized (-1..+1) amplitude sample for the left channel.
+     *
+     * @param buf
+     * @param frame
+     * @return
+     */
+    public static double getRight(byte[] buf, int frame) {
+        return (buf[frame * FRAME_SIZE + 2] << 8) | buf[frame * FRAME_SIZE + 3] & 0xFF;
+    }
 
     private AudioFormat getAudioFormat() {
         return new AudioFormat(
@@ -94,57 +153,4 @@ public class AudioInputOutputBase {
         return line;
     }
 
-    /**
-     * Write a normalized (-1..+1) amplitude sample to the left channel.
-     *
-     * @param buf
-     * @param frame
-     * @param value
-     */
-    public static void putLeft(byte[] buf, int frame, double value) {
-        if (Math.abs(value) > 1d) {
-            throw new IllegalArgumentException();
-        }
-        long wordValue = (long) (value * 32767d);
-        buf[frame * FRAME_SIZE + 0] = (byte) (wordValue >> 8);
-        buf[frame * FRAME_SIZE + 1] = (byte) (wordValue & 0xFF);
-    }
-
-    /**
-     * Write a normalized (-1..+1) amplitude sample to the right channel.
-     *
-     * @param buf
-     * @param frame
-     * @param value
-     */
-    public static void putRight(byte[] buf, int frame, double value) {
-        if (Math.abs(value) > 1d) {
-            throw new IllegalArgumentException();
-        }
-        long wordValue = (long) (value * 32767d);
-        buf[frame * FRAME_SIZE + 2] = (byte) (wordValue >> 8);
-        buf[frame * FRAME_SIZE + 3] = (byte) (wordValue & 0xFF);
-    }
-
-    /**
-     * Return the normalized (-1..+1) amplitude sample for the left channel.
-     * 
-     * @param buf
-     * @param frame
-     * @return
-     */
-    public static double getLeft(byte[] buf, int frame) {
-        return (buf[frame * FRAME_SIZE + 0] << 8) | buf[frame * FRAME_SIZE + 1] & 0xFF;
-    }
-
-    /**
-     * Return the normalized (-1..+1) amplitude sample for the left channel.
-     *
-     * @param buf
-     * @param frame
-     * @return
-     */
-    public static double getRight(byte[] buf, int frame) {
-        return (buf[frame * FRAME_SIZE + 2] << 8) | buf[frame * FRAME_SIZE + 3] & 0xFF;
-    }
 }
